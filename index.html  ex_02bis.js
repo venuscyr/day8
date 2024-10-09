506953e8af5e844f53e13d5a3418de8ab83879c1
venuscyr <venus.amako@epitech.digital>
2024-10-09 22:11:17 +0200

    index.html  ex_02bis.js

---
+++ index.html  ex_02bis.js
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Map of Epitech Digital Schools</title>
+    <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
+    <style>
+        #map { height: 670px; width: 100%; } 
+    </style>
+</head>
+<body>
+    <h1>Map of Epitech Digital Schools in France</h1>
+    <div id="map"></div>
+    
+    
+    <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
+    
+    <!-- External JavaScript file -->
+    <script src="ex_02.js"></script>
+</body>
+</html>
