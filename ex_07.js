282d79638162617c5cb7b34a08a3fc920f726c35
venuscyr <venus.amako@epitech.digital>
2024-10-09 19:22:56 +0200

    ex_07.js

---
+++ ex_07.js
@@ -0,0 +1,18 @@
+const mad = Vue.createMad({
+   data() {
+    return {
+        mouseX: 0,
+        mouseY: 0, 
+        imageX: window.innerWidth /2,
+        imageY: window.innerHeight /2
+    }
+   },
+   methods: {
+    updateMousePosition(event) {}
+   ,
+   moveImage() {}
+    
+   },
+   mounted() {}
+}) 
+ AudioParamMap.mount('#app')
